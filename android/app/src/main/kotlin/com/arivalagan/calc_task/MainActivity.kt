a489adb5ea3469f04c4a28171765cbb2647a9a5f
Arivalagan S <arivalagan.subramani@vanavil-systems.com>
2022-08-13 15:03:12 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/arivalagan/calc_task/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arivalagan.calc_task
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+const buttonBg = Color(0xFF313131);
+
